00dbee158f4448ee8a245394c23c5bcd14b9e413
Henrique X <hiqueaxavier@hotmail.com>
2020-08-25 20:26:42 -0300

    Correção exercício 2

--- EstruturaSequencial/2_NumeroInformado.py
+++ EstruturaSequencial/2_NumeroInformado.py
@@ -1,4 +1,4 @@
-# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+# 2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 
 n1 = input("Digite um número: ")
 print("O número informado foi ", n1)
