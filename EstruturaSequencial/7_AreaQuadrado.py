a4d3f29b265e40671ee264ed3cf2940ebdc929da
Henrique X <hiqueaxavier@hotmail.com>
2020-08-27 02:14:22 -0300

    Correção exercício 7

--- EstruturaSequencial/7_AreaQuadrado.py
+++ EstruturaSequencial/7_AreaQuadrado.py
@@ -4,5 +4,5 @@ lado = float(input("Digite a medida do lado do quadrado: "))
 area = lado * lado
 dobro = area * 2
 
-print("Área do quadrado = ", area)
-print("Dobro da área ", dobro)
+print("Área do quadrado =", area)
+print("Dobro da área =", dobro)
